e83219d6be6079c9c6d6038f177f68cc16df2a2d
Chris Thomson <chris@cthomson.ca>
2012-12-25 01:50:54 -0500

    Initial import.

---
+++ live_view.js
@@ -0,0 +1,38 @@
+(function() {
+	var map, markers = {}, lastUpdatedTime = 0;
+	  
+	function updateVehicleLocations() {
+		$.get('http://webservices.nextbus.com/service/publicXMLFeed?command=vehicleLocations&a=ttc&t=' + lastUpdatedTime, function(data) {
+			lastUpdatedTime = $(data).find('body lastTime').attr('time');
+  		  	
+			$(data).find('body vehicle').each(function(id, vehicle) {
+				if (!markers[$(vehicle).attr('id')]) {
+					markers[$(vehicle).attr('id')] = new google.maps.Marker({ map: map, title: $(vehicle).attr('routeTag') });
+				}
+				
+				var marker = markers[$(vehicle).attr('id')];
+				marker.setPosition(new google.maps.LatLng($(vehicle).attr('lat'), $(vehicle).attr('lon')));
+			});
+		});
+	}
+	  
+	function initialize() {
+		var mapOptions = {
+			zoom: 12,
+			center: new google.maps.LatLng(43.7172467450872, -79.37742082519532),
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		};
+		
+		map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+			
+		var updateInterval = 2000;
+		if (document.location.search.indexOf("update=") != -1) {
+			updateInterval = document.location.search.substr(document.location.search.indexOf("update=") + 7);
+		}
+		
+		updateVehicleLocations();
+		setInterval(updateVehicleLocations, updateInterval);
+	}
+	  
+	google.maps.event.addDomListener(window, 'load', initialize);
+})();
